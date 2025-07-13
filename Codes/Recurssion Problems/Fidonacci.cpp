64b95aad88af0c7592ba9eabf2d0f3eb3a98997c
Mallikarjun Paroji <mallikarjunparoji@Mallikarjuns-MacBook-Air.local>
2025-07-13 13:34:28 +0530

    Initial commit

---
+++ Codes/Recurssion Problems/Fidonacci.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int nthFibonacci(int n)
+{
+
+    if (n <= 1){
+        return n;
+    }
+return nthFibonacci(n - 1) + nthFibonacci(n - 2);
+}
+
+int main(){
+    int n = 5;
+    int result = nthFibonacci(n);
+    cout<< result << endl;
+
+    return 0;
+}
