55a3b4822dee75ff310ce17dc158c2e132fb4866
Mallikarjun-P-23 <mallikarjunparoji23@gmail.com>
2025-08-10 09:13:01 +0530

    k

---
+++ Codes/CompetativeProgramming/Geek/Minimize_height1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int getMinDiff(vector<int> &arr, int n, int k) {
+    if (n == 1) return 0;
+
+    sort(arr.begin(), arr.end());
+    int ans = arr[n - 1] - arr[0];
+
+    for (int i = 0; i < n - 1; ++i) {
+        int minVal = min(arr[0] + k, arr[i + 1] - k);
+        int maxVal = max(arr[n - 1] - k, arr[i] + k);
+
+        if (minVal < 0) continue; 
+
+        ans = min(ans, maxVal - minVal);
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<int> arr1 = {1, 5, 8, 10};
+    int n1 = arr1.size();
+    int k1 = 2;
+
+    cout << getMinDiff(arr1, n1, k1) << endl; 
+    return 0;
+}
